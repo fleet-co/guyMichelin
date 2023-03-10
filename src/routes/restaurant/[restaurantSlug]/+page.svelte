c3af64382b6e11e698eef350b6cd444a28543a8c
NellyBP <68547260+NellyBP@users.noreply.github.com>
2023-03-10 18:05:19 +0100

    oh que c'est laid

--- src/routes/restaurant/[restaurantSlug]/+page.svelte
+++ src/routes/restaurant/[restaurantSlug]/+page.svelte
@@ -3,32 +3,79 @@
   const restaurant = data.restaurant;
 </script>
 
-<h1>{restaurant?.name}</h1>
-<p>{restaurant?.address}</p>
+<img
+  class="imageResto"
+  src="https://www.bofrost.fr/medias/05236-FR-raviolis-au-fromage-tomate-et-basilic-pic1.jpg-W920xH575R1.6?context=bWFzdGVyfHByb2R1Y3QtaW1hZ2VzfDI1Njg3M3xpbWFnZS9qcGVnfHByb2R1Y3QtaW1hZ2VzL2hlMi9oMGQvOTE4NjE4OTA4MjY1NC5qcGd8Y2EyMWM1YmVmZWIxMWUxNzA5YmFhZTE5NmJjYTk2NGVkY2YwMmEyZDRjYWEyMGVlZjIwNzNlM2QyMjQzMDI2YQ"
+/>
 
-{#each restaurant?.reviews as review}
-  <div>
-    <span>Qualité</span>
-    <span>{review.rating_qualite}</span>
-  </div>
+<main>
+  <h1>{restaurant?.name}</h1>
+  <p>{restaurant?.address}</p>
+  <h2>Autres avis</h2>
+  {#each restaurant?.reviews as review}
+    <div>
+      <div class="review">
+        <div>Avis {review.users.firstname}</div>
+        <div class="critereWrapper">
+          <div class="critere">
+            😍
+            <div>
+              <span>{review.rating_qualite}</span>
+            </div>
+          </div>
+          <div class="critere">
+            💸
+            <div>
+              <span>{review.rating_prix}</span>
+            </div>
+          </div>
+          <div class="critere">
+            🫃🏻
+            <div>
+              <span>{review.rating_calage}</span>
+            </div>
+          </div>
+          <div class="critere">
+            😋
+            <div>
+              <span>{review.rating_regalade}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>"{review.comment}"</p>
+        </div>
+      </div>
+    </div>
+  {/each}
+</main>
 
-  <div>
-    <span>Prix</span>
-    <span>{review.rating_prix}</span>
-  </div>
+<style>
+  main {
+    padding: 24px;
+  }
+  .imageResto {
+    width: 100%;
+  }
 
-  <div>
-    <span>Calage</span>
-    <span>{review.rating_calage}</span>
-  </div>
-
-  <div>
-    <span>Régalade</span>
-    <span>{review.rating_regalade}</span>
-  </div>
-
-  <div>
-    <p>"{review.comment}"</p>
-    <div>{review.users.firstname}</div>
-  </div>
-{/each}
+  h2 {
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .critereWrapper {
+    display: flex;
+    gap: 8px;
+  }
+  .review {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+  .critere {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
