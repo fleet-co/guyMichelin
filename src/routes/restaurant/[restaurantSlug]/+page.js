256c03d09a000666e49e9f8fbf788b6d6789f59e
NellyBP <68547260+NellyBP@users.noreply.github.com>
2023-03-10 15:25:23 +0100

    Update +page.js

--- src/routes/restaurant/[restaurantSlug]/+page.js
+++ src/routes/restaurant/[restaurantSlug]/+page.js
@@ -1,15 +1,3 @@
-// @ts-nocheck
-import { dev } from '$app/environment';
-
-// we don't need any JS on this page, though we'll load
-// it in dev so that we get hot module replacement
-export const csr = dev;
-
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = false;
-
-
 import { createClient } from "@supabase/supabase-js";
 
 /** @type {import('./$types').PageLoad} */
